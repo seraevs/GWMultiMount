a1e80eb7342293c85a50ce94a5c8921b4af38ce2
seraevs <54232994+seraevs@users.noreply.github.com>
2021-09-11 19:42:37 +0300

    Add files via upload

---
+++ MultiScan-1/Net_Info.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiScan_1
+{
+    public partial class Net_Info : Form
+    {
+        public Net_Info()
+        {
+            InitializeComponent();
+            ViewInfoNet();
+        }
+
+        private void btnBackToMain_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ViewInfoNet()
+        {
+            // Add vertical scroll bars to the TextBox control.
+            txt_Info_Net.ScrollBars = ScrollBars.Vertical;
+
+            if (File.Exists(@"net_ifo.txt"))
+            {
+                string text = System.IO.File.ReadAllText(@"net_ifo.txt");
+
+                // Display the file contents. Variable text is a string.
+                txt_Info_Net.Text = text;
+            }
+            else MessageBox.Show("File " + @"net_ifo.txt" + " not exists");
+        }
+    }
+}
